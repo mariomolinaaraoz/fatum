73dd53a7575ac026675d082ecc8d3b909f1f8641
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-03-14 17:36:08 -0300

    avance 8

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+//Función que arma el lighbox
+
 const images = document.querySelectorAll('.image');
 const lightbox = document.querySelector('.lightbox');
 const lightboxImg = lightbox.querySelector('img');
