3a0f3d48466b47f5f6d923f0b33a95d7adc6b250
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-03-28 14:13:47 -0300

    avance 21

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
+  setTimeout(showSlides, 5000); // Change image every 4 seconds
 }
 
 //Función que arma el lighbox
