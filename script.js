ee8d48c7013854981686bfbe85a58694c4d925c8
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-04-12 07:47:04 -0300

    avance 24

--- script.js
+++ script.js
@@ -55,27 +55,6 @@ function adjustImageSize() {
   });
 }
 
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
-function seleccionar(link) {
-  var opciones = document.querySelectorAll("#links  a");
-  opciones[0].className = "";
-  opciones[1].className = "";
-  opciones[2].className = "";
-  opciones[3].className = "";
-  opciones[4].className = "";
-  link.className = "seleccionado";
-}
-
-//función que muestra el menu responsive
-function responsiveMenu() {
-  var x = document.getElementById("nav");
-  if (x.className === "") {
-    x.className = "responsive";
-  } else {
-    x.className = "";
-  }
-}
-
 //función que envia el correo desde contacto
 const btn = document.getElementById("button_form_contact");
 document
